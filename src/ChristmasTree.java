527489ca6b7449ea8031145285914ad9a19eea59
Bridson.Dylan <Bridson.Dylan@SEMS-259-34.SEMS259.LAB>
2017-12-20 14:55:43 -0800

    Christmas Tree

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -5,12 +5,14 @@
  *    
  ******************************************************************************/
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 
 public class ChristmasTree {
 
-	Robot rob  = new Robot();
+	Robot rob  = new Robot("batman");
 	
     public static void main(String[] args) {
         ChristmasTree ohChristmasTree = new ChristmasTree();
@@ -26,13 +28,24 @@ public class ChristmasTree {
 
     void drawTreeBody() {
         // 8. Change the color of the line the robot draws to forest green    
-
-
-        // 1. Make a variable for turnAmount and set it to 175
-
+rob.setPenColor(Color.green);
+rob.penDown();
+int bob=175;
+        // 1. Make a variable for turnAmount and set it to 175\
+rob.setSpeed(100);
+rob.setAngle(90);
+for (int i = 0; i < 11; i++) {
+rob.move((int) treeWidth);
+rob.turn(bob);
+treeWidth=treeWidth*scale;
+rob.move((int) treeWidth);
+rob.turn(-bob);
+treeWidth=treeWidth*scale;
+bob=bob-1;
+}
 
         // 2. Start the Robot facing to the right
-
+		
 
         // 5. Repeat steps 3 through 11, 11 times
 
@@ -56,24 +69,32 @@ public class ChristmasTree {
     
     void drawTreeTrunk() {
         // 1. Move the robot half the width of the tree
-
+rob.move((int) (treeWidth/2));
 
         // 2. Change the robot so that it is pointing straight down
-
+rob.turn(90);
 
         // 4. Set the pen width to the tree width divided by 10
+rob.setPenWidth((int) (treeWidth/10));
 
 
         // 5. Change the color of the line the robot draws to brown
-
+rob.setPenColor(240,100,100);
 
         // 3. Move the robot a quarter the tree width
-
+rob.move((int) (treeWidth/4));
 
     }
     
     void drawStar() {
         // * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a star.
+    	for (int i = 0; i < 5; i++) {
+		rob.setSpeed(50);
+		rob.penDown();
+    		
+		rob.move(50);
+		rob.turn(144);	}
+    	
     
     }
 
